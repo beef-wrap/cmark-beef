0db80d0aeb9d6324202381203b9da67f64fa3238
unknown <farismmk@gmail.com>
2025-03-28 03:13:44 -0700

    use xbuild

--- build.sh
+++
@@ -1,5 +0,0 @@
-cmake -S . -B cmark/build -DCMAKE_BUILD_TYPE=Debug
-cmake --build cmark/build --config Debug
-
-cmake -S . -B cmark/build -DCMAKE_BUILD_TYPE=Release
-cmake --build cmark/build --config Release
